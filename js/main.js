668de2cfbe3a25f3a6e9beee00426f8e30d769da
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-11-11 22:02:39 -0500

    bug fix with tabs & banner & 3rd slider

--- js/main.js
+++ js/main.js
@@ -9,14 +9,16 @@ $(function () {
 
   $(".tab").on("click", function (e) {
     e.preventDefault();
-    $(".tab").removeClass("tab--active");
-    $(".tabs-content").removeClass("tabs-content--active");
+    $($(this).siblings()).removeClass("tab--active");
+    $($(this).parent().siblings().find("div")).removeClass(
+      "tabs-content--active"
+    );
     $(this).addClass("tab--active");
     $($(this).attr("href")).addClass("tabs-content--active");
   });
 
   $(".product__item-favorite").on("click", function () {
-    $(".product__item-favorite").toggleClass("product__item-favorite--active");
+    $(this).toggleClass("product__item-favorite--active");
   });
 
   $(".product__slider").slick({
